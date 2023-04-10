cf09b96f50a66742d8ed6a230f4c5c05da16a23c
Ahmed Omar <ahmmedd606@gmail.com>
2023-04-10 14:05:37 +0200

    update lectures

--- routes/guest-api.php
+++ routes/guest-api.php
@@ -11,26 +11,4 @@ use Twilio\Rest\Client;
 
 Route::get('login',[LoginController::class,'login']);
 Route::post('whatsapp/send-test-message', function (){return "sdfds";});
-Route::get('update-billings',function (){
-    $lectures = \App\Models\Lecture::all();
-    foreach ($lectures as $lecture){
-
-        $course = Course::find($lecture->course_id);
-
-        $billing['tutor_id'] = $course->tutor_id;
-        $billing['family_id'] = $course->family_id;
-        $billing['lecture_id'] = $lecture->id;
-        $billing['currency_id'] = Family::find($billing['family_id'])->currency->id;
-        $billing['hour_rate'] = Family::find($billing['family_id'])->hour_price;
-        $billing['lecture_duration'] = $lecture->lecture_duration;
-        $billing['month'] = Carbon::now()->month;
-        $billing['year'] = Carbon::now()->year;
-        $billing['amount'] = $billing['lecture_duration']*$billing['hour_rate'];
-
-        Billing::create($billing);
-
-    }
-    return response()->json(['status'=>true]);
-
-});
 
