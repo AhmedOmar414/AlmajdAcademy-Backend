b5cb024ed68549f75520376c2243b3cf8e33de33
Ahmed Omar <ahmmedd606@gmail.com>
2023-04-24 21:43:32 +0200

    paid

--- app/Http/Controllers/Api/Admin/Paypal/BillingsController.php
+++ app/Http/Controllers/Api/Admin/Paypal/BillingsController.php
@@ -62,4 +62,30 @@ class BillingsController extends Controller
         return response()->json(['message'=>'تم استرجاع الفواتير بنجاح','data'=>$allPayments,'code'=>200]);
 
     }
+
+    public function usersBillingsPaid(Request $request){
+        $request->validate([
+            'month'=>'required'
+        ]);
+        $month = $request->month;
+        $families = Family::all();
+        $allPayments = [];
+        $userObject = [];
+        foreach ($families as $family) {
+            $unpaidBillings = Billing::where('family_id',$family->id)->where('is_paid',1)->where('month',$request->month)->get();
+            $totalAmount = $unpaidBillings->sum('amount');
+            if ($totalAmount == 0){
+                continue;
+            }else{
+                $userObject['user_id'] = $family->id;
+                $userObject['mobile_number'] = $family->whatsapp_number;
+                $userObject['family_name'] = $family->name;
+                $userObject['currency_name'] = $family->currency->name;
+                $userObject['currency_sumbol'] = $family->currency->symbol;
+                $userObject['totalAmount'] = $totalAmount;
+                array_push($allPayments,$userObject);
+            }
+        }
+        return response()->json(['message'=>'تم استرجاع الفواتير بنجاح','data'=>$allPayments,'code'=>200]);
+    }
 }
